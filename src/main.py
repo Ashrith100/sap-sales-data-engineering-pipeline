cfbf78149a14596de06418ff72873a863a823944
akanala <ashrith-kanala@uiowa.edu>
2026-05-18 21:19:23 -0500

    Add missing value validation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from extract.read_file import read_excel_file
 from transform.clean_columns import clean_column_names
 from validate.check_duplicates import find_duplicates
+from validate.check_missing_values import find_missing_required_values
 
 
 def main():
@@ -21,6 +22,17 @@ def main():
 
     if not duplicates.empty:
         print(duplicates[["vbeln", "posnr"]].head())
+
+    missing_values = find_missing_required_values(df)
+
+    print(f"\nRecords Missing Required Values: {len(missing_values)}")
+
+    if not missing_values.empty:
+        print(
+            missing_values[
+                ["vbeln", "posnr", "fkimg", "vrkme", "meins"]
+            ].head()
+        )
 
 
 if __name__ == "__main__":
